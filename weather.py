cd1950da65109f59ae6501990afd113c18015563
EvanGerasimenko <jonikkipie@gmail.com>
2020-06-07 16:59:00 +0300

    language
    поменял язык

--- weather.py
+++ weather.py
@@ -1,5 +1,5 @@
-import pyowm 
-owm = pyowm.OWM ('92105564edd301e393450045b4457058')
+import pyowm
+owm = pyowm.OWM ('92105564edd301e393450045b4457058', language = "ru")
 
 
 while True:
@@ -18,10 +18,9 @@ while True:
     print ("in city of " + place + " is " + w.detailed_status + " right now")
     print ("temperature right now is " + str(temp))
 
-    if temp < 10: 
+    if temp < 10:
 	    print ("it's cold right now, get warm clothes")
     elif temp < 25:
 	    print ("normal temperature")
     else:
 	    print ("temperature is good for light clothes")
-
